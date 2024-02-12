f68a48fc5a5bf8d59109a9e9150c7881a2a10963
ClementRollin <clemrollin01@gmail.com>
2024-02-12 22:14:39 +0100

    Application Pokedeck

--- src/components/EvolutionPopup.tsx
+++ src/components/EvolutionPopup.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Pokemon} from "./PokeList.tsx";
+import { Pokemon } from './PokeList';
 
 interface EvolutionPopupProps {
     pokemonId: string;
@@ -8,38 +8,54 @@ interface EvolutionPopupProps {
     onClose: () => void;
 }
 
+interface EvolutionNode {
+    species: {
+        name: string;
+        url: string;
+    };
+    evolves_to: EvolutionNode[];
+}
+
 interface EvolutionDetail {
     name: string;
     imageUrl: string;
 }
 
-const EvolutionPopup = ({ pokemonId, onClose, pokemonDetails }: EvolutionPopupProps) => {
+const EvolutionPopup: React.FC<EvolutionPopupProps> = ({ pokemonId, pokemonDetails, onClose }) => {
     const [evolutionDetails, setEvolutionDetails] = useState<EvolutionDetail[]>([]);
 
     useEffect(() => {
         const fetchEvolutionChain = async () => {
             try {
-                const idMatch = pokemonId.match(/pokemon\/(\d+)\//);
+                // Extrait l'ID numérique de la chaîne d'évolution depuis l'URL passée en props
+                const idMatch = pokemonId.match(/\/(\d+)\//);
                 if (idMatch) {
                     const id = idMatch[1];
-                    const response = await axios.get(`https://pokeapi.co/api/v2/evolution-chain/${id}`);
-
-                    const evolutions = [];
-                    let currentEvolution = response.data.chain;
-                    while (currentEvolution && currentEvolution.species) {
-                        const evolutionResponse = await axios.get(currentEvolution.species.url);
-                        const imageUrl = evolutionResponse.data.sprites?.front_default || 'default-image-url.png';
-                        evolutions.push({
-                            name: evolutionResponse.data.name,
-                            imageUrl: imageUrl
-                        });
-                        currentEvolution = currentEvolution.evolves_to[0];
-                    }
-                    setEvolutionDetails(evolutions);
+                    const { data } = await axios.get(`https://pokeapi.co/api/v2/evolution-chain/${id}/`);
+                    await processEvolutionChain(data.chain);
                 }
             } catch (error) {
                 console.error("Erreur lors de la récupération de la chaîne d'évolution:", error);
             }
+        };
+
+        const processEvolutionChain = async (evolutionNode: EvolutionNode) => {
+            let evolutions: EvolutionDetail[] = [];
+            let currentEvolution: EvolutionNode | null = evolutionNode;
+
+            while (currentEvolution) {
+                const speciesUrl = currentEvolution.species.url;
+                const speciesData = await axios.get(speciesUrl);
+                const pokemonData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${speciesData.data.name}`);
+                evolutions.push({
+                    name: pokemonData.data.name,
+                    imageUrl: pokemonData.data.sprites.front_default || 'default-image-url.png',
+                });
+
+                currentEvolution = currentEvolution.evolves_to[0] || null;
+            }
+
+            setEvolutionDetails(evolutions);
         };
 
         fetchEvolutionChain();
@@ -50,21 +66,18 @@ const EvolutionPopup = ({ pokemonId, onClose, pokemonDetails }: EvolutionPopupPr
             <h2>Détails du Pokémon</h2>
             <div>
                 <h3>{pokemonDetails.name}</h3>
-                <img src={pokemonDetails.imageUrl} alt={pokemonDetails.name}/>
+                <img src={pokemonDetails.imageUrl} alt={pokemonDetails.name} />
                 <p>Types: {pokemonDetails.types.join(', ')}</p>
             </div>
             <h2>Évolutions</h2>
-            {evolutionDetails.length > 0 ? (
-                evolutionDetails.map((evolution, index) => (
-                    <div key={index}>
-                        <h4>Évolution {index + 1} : </h4>
-                        <img src={evolution.imageUrl} alt={evolution.name}/>
-                        <p>{evolution.name}</p>
+            <div className="evolutionPokemon">
+                {evolutionDetails.map((evolution, index) => (
+                    <div key={index} className="evolution-item">
+                        <h4>{evolution.name}</h4>
+                        <img src={evolution.imageUrl} alt={evolution.name} />
                     </div>
-                ))
-            ) : (
-                <p>Chargement des évolutions...</p>
-            )}
+                ))}
+            </div>
             <button onClick={onClose}>Fermer</button>
         </div>
     );
