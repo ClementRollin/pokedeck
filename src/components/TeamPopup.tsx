cc9d734b7d692b1adff995c993c729c0894881ae
ClementRollin <clemrollin01@gmail.com>
2024-03-25 11:59:59 +0100

    Application Desktop - Pokedex

--- src/components/TeamPopup.tsx
+++ src/components/TeamPopup.tsx
@@ -10,21 +10,24 @@ const TeamPopup: React.FC<TeamPopupProps> = ({ team, pokemons, onClose }) => {
 
     
     return (
-        <div className="teamPopup">
-            <h2>Mon équipe</h2>
-            <div className="teamGrid">
-                {team.map(pokemonName => {
-                    const pokemon = pokemons.find(p => p.name === pokemonName);
-                    return pokemon ? (
-                        <div key={pokemonName}>
-                            <img src={pokemon.imageUrl} alt={pokemonName} />
-                            <p>{pokemonName}</p>
-                        </div>
-                    ) : null;
-                })}
+        <>
+            <div className="backdrop" onClick={onClose}></div>
+            <div className="teamPopup">
+                <h2>Mon équipe</h2>
+                <div className="teamGrid">
+                    {team.map(pokemonName => {
+                        const pokemon = pokemons.find(p => p.name === pokemonName);
+                        return pokemon ? (
+                            <div key={pokemonName}>
+                                <img src={pokemon.imageUrl} alt={pokemonName} />
+                                <p>{pokemonName}</p>
+                            </div>
+                        ) : null;
+                    })}
+                </div>
+                <button onClick={onClose}>Fermer</button>
             </div>
-            <button onClick={onClose}>Fermer</button>
-        </div>
+        </>
     );
 };
 
